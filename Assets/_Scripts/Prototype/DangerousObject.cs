37d4af9233f99dd0db502abe050d584304031d4e
Brian Fann <fannbrian@gmail.com>
2018-06-16 18:53:29 -0700

    Added health manager/dangerous obj prefab

---
+++ Assets/_Scripts/Prototype/DangerousObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Prototype
+{
+	public class DangerousObject : MonoBehaviour
+	{
+		public void OnCollisionEnter2D(Collision2D collision)
+		{
+			var obj = collision.gameObject;
+
+			if (obj == GlobalData.s.MainPlayer) {
+				
+			}
+		}
+	}
+}
---
+++ Assets/_Scripts/Prototype/HealthManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Prototype
+{
+	/// <summary>
+    /// Manages the player's health
+    /// </summary>
+	public class HealthManager : MonoBehaviour
+	{
+		// Singleton
+		public static HealthManager s;
+
+        public int MaxHealth;
+		public int CurrentHealth { get; private set; }
+
+        public delegate void DamageEvent();
+		public static event DamageEvent OnDamage;
+
+        public delegate void DeathEvent();
+        public static event DeathEvent OnDeath;
+
+        // Set singleton
+		void Awake()
+		{
+			s = this;
+		}
+
+        /// <summary>
+        /// Lower player's health by amount
+        /// </summary>
+        /// <param name="amount">Amount.</param>
+		public void DamagePlayer(int amount) {
+			CurrentHealth -= amount;
+         
+			if (CurrentHealth <= 0) {
+                CurrentHealth = 0;
+				OnDamage();
+			}
+			else {
+				OnDeath();
+			}
+		}
+	}
+}
