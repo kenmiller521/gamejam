5e2e0d0d09a00155877abdaef91925bd8045fbc0
Brian Fann <fannbrian@gmail.com>
2018-06-16 17:33:42 -0700

    + Prototype

---
+++ Assets/_Scripts/Prototype/PlayerManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+namespace Prototype
+{
+	/// <summary>
+	/// Handles player movement mechanics
+	/// </summary>
+	public class PlayerManager : MonoBehaviour
+	{
+		// Singleton
+		public static PlayerManager s;
+
+		// Inspector values
+		public float HorizontalSpeed;
+		public float JumpForce;
+
+		// Rigidbody References
+        Rigidbody2D CurrentRB2D
+        {
+            get
+            {
+                return GlobalData.s.IsInverted ? InvertedRB2D : MainRB2D;
+            }
+		}
+        Rigidbody2D OtherRB2D
+        {
+            get
+            {
+                return GlobalData.s.IsInverted ? MainRB2D : InvertedRB2D;
+            }
+        }
+
+		Rigidbody2D MainRB2D;
+		Rigidbody2D InvertedRB2D;
+
+		#region MonoBehaviour Callbacks
+
+        /// <summary>
+        /// Set singleton
+        /// </summary>
+		void Awake()
+		{
+			s = this;
+		}
+
+        /// <summary>
+        /// Subscribe to each event
+        /// </summary>
+		void Start()
+		{
+			InputManager.OnMove += OnMove;
+			InputManager.OnJump += OnJump;
+			MainRB2D = GlobalData.s.MainPlayer.GetComponent<Rigidbody2D>();
+			InvertedRB2D = GlobalData.s.InvertedPlayer.GetComponent<Rigidbody2D>();
+		}
+
+		void Update()
+		{
+			var pos = GlobalData.s.CurrentPlayer.transform.position;
+			pos.y *= -1;
+			GlobalData.s.OtherPlayer.transform.position = pos;
+		}
+
+		#endregion
+
+		#region Event Callbacks
+
+		public void OnMove(float x)
+		{
+			var yVelocity = CurrentRB2D.velocity.y;
+			var velocity = new Vector2(x * HorizontalSpeed, yVelocity);
+			Debug.Log(GlobalData.s.CurrentPlayer);
+			CurrentRB2D.velocity = velocity;
+		}
+
+		public void OnJump()
+		{
+			var jumpVelocity = new Vector2(0, JumpForce * GlobalData.s.InvertMultiplier);
+			CurrentRB2D.velocity += jumpVelocity;
+		}
+
+		#endregion
+	}
+}
